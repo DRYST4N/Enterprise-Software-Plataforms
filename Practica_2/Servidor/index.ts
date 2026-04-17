84708304b6793567dddc41e5d836d67762db0730
DRYST4N <diegomorasol31@gmail.com>
2026-04-17 12:29:16 +0200

    ´cambios´

--- Practica_2/Servidor/index.ts
+++ Practica_2/Servidor/index.ts
@@ -1,23 +1,23 @@
 import express from 'express'
 import cors from 'cors'
-//import movieRoutes from './routes/movie.route'
-//import theaterRoutes from './routes/theater.route'
+import movieRoutes from './routes/movie.route'
+import theaterRoutes from './routes/theater.route'
 
 
 const app = express();
 
 
-//Middleware esenciales
+// Middleware esenciales
 app.use(cors());
 app.use(express.json());
 
-//Rutas
-//app.use('/api/movies', movieRoutes);
-//app.use('/api/theaters', theaterRoutes);
+// Rutas
+app.use('/api/movies', movieRoutes);
+app.use('/api/theaters', theaterRoutes);
 
 
-//Inicio del servidor
-const PORT =3000;
+// Inicio del servidor
+const PORT = 3000;
 app.listen(PORT, () => {
-    console.log('Servidor lanzado en el puerto 3000')
-})
+    console.log('Servidor lanzado en el puerto 3000');
+});
