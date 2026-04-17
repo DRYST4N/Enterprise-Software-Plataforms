b1ab26f169270612392aacd5d11efd5ca73e7731
AdrianEunoia <adriangarciainbox@gmail.com>
2026-04-17 11:43:13 +0200

    fix: npm run dev

--- Practica_2/Servidor/routes/movie.route.ts
+++ Practica_2/Servidor/routes/movie.route.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
-import * as MovieController from '../controllers/movie.controller';
-import { validate } from '../middlewares/validate.middlewares';
-import { FilterByDateSchema, SearchByActorSchema } from '../schemas/movie.schemas';
+// Fíjate en el .js al final de cada ruta local
+import * as MovieController from '../controllers/movie.controller.js';
+import { validate } from '../middlewares/validate.middlewares.js';
+import { FilterByDateSchema, SearchByActorSchema } from '../schemas/movie.schemas.js';
 
 const router = Router();
 
 router.get('/', MovieController.getAllMovies);
-router.get('/search', validate(SearchByActorSchema),MovieController.getMoviesByActor);
+router.get('/search', validate(SearchByActorSchema), MovieController.getMoviesByActor);
 router.get('/by-date', validate(FilterByDateSchema), MovieController.getMoviesByDate);
 
 export default router;
