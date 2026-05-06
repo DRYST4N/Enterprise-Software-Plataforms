5464fcc1a43e5360a88b19011a4185cd2e93c8ae
DRYST4N <diegomorasol31@gmail.com>
2026-05-06 10:47:32 +0200

    Los modules

--- Practica_final/Backend/services/festival.service.ts
+++ Practica_final/Backend/services/festival.service.ts
@@ -1,9 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { FestivalDTO } from '../dtos/festival.dto';
-
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
-const prisma = new PrismaClient({ adapter });
+import { FestivalDTO } from "../dtos/festival.dto";
+import prisma from '../lib/prisma';
 
 export class FestivalService {
     static async getAll() {
